096dedd13c990d3310073184650230b9df5a12c4
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2025-08-22 20:54:39 -0300

    feat: change logo

--- client/src/shared/ClientLayout/components/Navbar.tsx
+++ client/src/shared/ClientLayout/components/Navbar.tsx
@@ -26,12 +26,9 @@ const Navbar = () => {
    return (
       <header className="bg-white border-b border-gray-200 shadow-xs h-16 flex items-center">
          <div className="container mx-auto px-4">
-            <div className="flex  justify-between ">
+            <div className="flex justify-between ">
                <Link to="/" className="flex items-center space-x-2">
-                  <div className="w-8 h-8 bg-blue-600 rounded-lg flex items-center justify-center">
-                     <Bubbles className="w-5 h-5 text-white" />
-                  </div>
-                  <span className="text-xl font-bold text-gray-900">AQUAZUL</span>
+                  <img src="/aquazul-logo.png" className="h-8" alt="AQUAZUL Logo" />
                </Link>
 
                {/* Desktop Navigation */}
