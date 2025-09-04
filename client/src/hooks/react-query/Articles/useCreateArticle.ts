abb8b6906926319263351efbf6374290b4bea17e
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2025-09-04 20:33:55 -0300

    feat: cache update for articles creation

--- client/src/hooks/react-query/Articles/useCreateArticle.ts
+++ client/src/hooks/react-query/Articles/useCreateArticle.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { extractErrorData } from '@utils/extractErrorDetails'
 import { createArticle } from '@services/articles.service'
+import type { Article } from '@models/Article.model'
 import { queriesKeys } from '@config/reactQueryKeys'
 import { toast } from 'sonner'
 
@@ -9,12 +10,42 @@ const useCreateArticle = () => {
 
    const { mutateAsync: createArticleMutate, isPending } = useMutation({
       mutationFn: createArticle,
-      onSuccess: ({ message }) => {
+      onSuccess: ({ message, article }) => {
          toast.success(message)
 
-         queryClient.invalidateQueries({
-            queryKey: [queriesKeys.FETCH_ARTICLES],
-         })
+         queryClient.setQueryData(
+            [queriesKeys.FETCH_ARTICLES],
+            (old: { articles: Article[]; categories: Record<string, string> }) => {
+               if (!old) return old
+
+               // Agregar el nuevo artículo al array
+               const updatedArticles = [article, ...old.articles]
+
+               let updatedCategories = old.categories
+
+               // Si la categoría no existe en el objeto categories, agregarla
+               if (article.category && !old.categories[article.category.id]) {
+                  // Agregar la nueva categoría
+                  updatedCategories = {
+                     ...old.categories,
+                     [article.category.id]: article.category.name,
+                  }
+
+                  // Ordenar las categorías alfabéticamente por nombre
+                  const sortedEntries = Object.entries(updatedCategories).sort(
+                     ([, aName], [, bName]) => aName.localeCompare(bName)
+                  )
+
+                  updatedCategories = Object.fromEntries(sortedEntries)
+               }
+
+               return {
+                  ...old,
+                  articles: updatedArticles,
+                  categories: updatedCategories,
+               }
+            }
+         )
       },
       onError: (error) => {
          if (error?.message === 'Network Error') return
