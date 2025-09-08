3b1d69a0327deeb60d972e594307b294289dcc0b
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2025-09-08 01:21:45 -0300

    feat: update info banner for errors

--- client/src/shared/InfoBanner.tsx
+++ client/src/shared/InfoBanner.tsx
@@ -1,35 +1,55 @@
 import { ChevronDown, Info } from 'lucide-react'
 import { useState } from 'react'
+import { cn } from '@shadcn'
 
 interface InfoBannerProps {
    title: string
    description: string[]
+   withDropdown?: boolean
+   mode?: 'info' | 'error'
 }
 
-const InfoBanner = ({ title, description }: InfoBannerProps) => {
+const InfoBanner = ({
+   title,
+   description,
+   mode = 'info',
+   withDropdown,
+}: InfoBannerProps) => {
    const [showBanner, setShowBanner] = useState(false)
+   const isOpen = withDropdown ? showBanner : true
 
    return (
-      <div className="text-blue-800 p-4 bg-gradient-to-r from-blue-50 to-indigo-50 border-l-4 border-blue-600 rounded-r-md">
+      <div
+         className={`p-4 border-l-4 rounded-r-md ${
+            mode === 'info'
+               ? 'text-blue-800 bg-gradient-to-r from-blue-50 to-indigo-50 border-blue-600'
+               : 'text-red-800 bg-gradient-to-r from-rose-50 to-red-50 border-red-600'
+         }`}
+      >
          <div
-            onClick={() => setShowBanner(!showBanner)}
-            className="flex items-center justify-between cursor-pointer"
+            onClick={withDropdown ? () => setShowBanner((prev) => !prev) : undefined}
+            className={cn(
+               'flex items-center justify-between',
+               withDropdown && 'cursor-pointer'
+            )}
          >
             <div className="flex items-center gap-3">
                <Info className="size-5 flex-shrink-0" />
                <h4 className="font-medium ">{title}</h4>
             </div>
 
-            <ChevronDown
-               className={`size-4 text-blue-600 transition-transform duration-300 ${
-                  showBanner && 'rotate-180'
-               }`}
-            />
+            {withDropdown && (
+               <ChevronDown
+                  className={`size-4 text-blue-600 transition-transform duration-300 ${
+                     isOpen && 'rotate-180'
+                  }`}
+               />
+            )}
          </div>
 
          <div
             className={`overflow-hidden transition-all duration-600! ${
-               showBanner ? 'max-h-[500px]' : 'max-h-0'
+               isOpen ? 'max-h-[500px]' : 'max-h-0'
             }`}
          >
             <ul className="text-sm px-8 list-disc list-inside space-y-1 mt-2">
