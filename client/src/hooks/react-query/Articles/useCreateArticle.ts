f2895350a3a48606cf31172ccd6d27e8ead507f8
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2025-09-12 00:12:34 -0300

    refactor: simplify success handling in article creation and deletion

--- client/src/hooks/react-query/Articles/useCreateArticle.ts
+++ client/src/hooks/react-query/Articles/useCreateArticle.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { extractErrorData } from '@utils/extractErrorDetails'
 import { createArticle } from '@services/articles.service'
-import type { Article } from '@models/Article.model'
 import { queriesKeys } from '@config/reactQueryKeys'
 import { toast } from 'sonner'
 
@@ -10,42 +9,10 @@ const useCreateArticle = () => {
 
    const { mutateAsync: createArticleMutate, isPending } = useMutation({
       mutationFn: createArticle,
-      onSuccess: ({ message, article }) => {
+      onSuccess: ({ message }) => {
          toast.success(message)
 
-         queryClient.setQueryData(
-            [queriesKeys.FETCH_ARTICLES],
-            (old: { articles: Article[]; categories: Record<string, string> }) => {
-               if (!old) return old
-
-               // Agregar el nuevo artículo al array
-               const updatedArticles = [article, ...old.articles]
-
-               let updatedCategories = old.categories
-
-               // Si la categoría no existe en el objeto categories, agregarla
-               if (article.category && !old.categories[article.category.id]) {
-                  // Agregar la nueva categoría
-                  updatedCategories = {
-                     ...old.categories,
-                     [article.category.id]: article.category.name,
-                  }
-
-                  // Ordenar las categorías alfabéticamente por nombre
-                  const sortedEntries = Object.entries(updatedCategories).sort(
-                     ([, aName], [, bName]) => aName.localeCompare(bName)
-                  )
-
-                  updatedCategories = Object.fromEntries(sortedEntries)
-               }
-
-               return {
-                  ...old,
-                  articles: updatedArticles,
-                  categories: updatedCategories,
-               }
-            }
-         )
+         queryClient.invalidateQueries({ queryKey: [queriesKeys.FETCH_ARTICLES] })
       },
       onError: (error) => {
          if (error?.message === 'Network Error') return
--- client/src/hooks/react-query/Articles/useDeleteArticle.ts
+++ client/src/hooks/react-query/Articles/useDeleteArticle.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { extractErrorData } from '@utils/extractErrorDetails'
 import { deleteArticle } from '@services/articles.service'
-import type { Article } from '@models/Article.model'
 import { queriesKeys } from '@config/reactQueryKeys'
 import { toast } from 'sonner'
 
@@ -10,37 +9,10 @@ function useDeleteArticle() {
 
    const { mutateAsync: deleteArticleMutate, isPending } = useMutation({
       mutationFn: deleteArticle,
-      onSuccess: ({ message, article, category }) => {
+      onSuccess: ({ message }) => {
          toast.success(message)
 
-         //hay dos opciones, invalidar la querry o actualizar el cache manualmente
-         // queryClient.invalidateQueries({ queryKey: [queriesKeys.FETCH_ARTICLES] })
-         queryClient.setQueryData(
-            [queriesKeys.FETCH_ARTICLES],
-            (old: { articles: Article[]; categories: Record<string, string> }) => {
-               if (!old) return old
-
-               // Actualizar el array de artículos
-               const updatedArticles = old.articles.filter(
-                  (oldArticle) => oldArticle.id !== article.id
-               )
-
-               // Si se eliminó la categoría, también actualizar el array de categorías
-               const updatedCategories = category
-                  ? Object.fromEntries(
-                       Object.entries(old.categories).filter(
-                          ([keyId]) => keyId !== category.id
-                       )
-                    )
-                  : old.categories
-
-               return {
-                  ...old,
-                  articles: updatedArticles,
-                  categories: updatedCategories,
-               }
-            }
-         )
+         queryClient.invalidateQueries({ queryKey: [queriesKeys.FETCH_ARTICLES] })
       },
       onError: (error) => {
          if (error?.message === 'Network Error') return
